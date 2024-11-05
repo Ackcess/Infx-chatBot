4c2707f855e5d7d8ed96a143fbe1d1ffc37e371e
Apolly <83222827+Ackcess@users.noreply.github.com>
2024-11-05 23:39:19 +0300

    -chat

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def chat():
 
 def get_Chat_response(text):
     print("text:",text)
-    text = text + '[ Note: answer the event going on is crdb chat bot launching day and answer only crdb bank related questions, if you are asked so] use less than 20 words' 
+    text = text + '[ Note: answer the say about imperial marketing and innovation compay in tanzania only related questions, if you are asked so] use less than 20 words' 
     model = genai.GenerativeModel('gemini-1.0-pro-001')
     chat = model.start_chat(history=[])
     response = chat.send_message(text)
